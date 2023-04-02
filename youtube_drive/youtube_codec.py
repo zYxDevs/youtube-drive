372612528c4f4548af37c4ba53c0830a43679309
Sourcery AI <>
2023-04-02 05:48:54 +0000

    'Refactored by Sourcery'

--- youtube_drive/youtube_codec.py
+++ youtube_drive/youtube_codec.py
@@ -33,48 +33,46 @@ def decrypt_data_aes(data: bytes, key: bytes) -> bytes:
     nonce, tag, ciphertext = raw[:16], raw[16:32], raw[32:]
 
     cipher = AES.new(key, AES.MODE_EAX, nonce)
-    clear_data = cipher.decrypt_and_verify(ciphertext, tag)
-    return clear_data
+    return cipher.decrypt_and_verify(ciphertext, tag)
 
 
 def encode(infile_path, outvideo_path, encrypt=ENABLE_ENCRYPTION, key=KEY,
            fps=20, num_cols_per_frame=64, num_rows_per_frame=36):
-    fd = open(infile_path, 'rb')
-    raw_data_bytes = fd.read()
-    if encrypt:
-        raw_data_bytes = encrypt_data_aes(raw_data_bytes, key)
-    data_bytes = np.frombuffer(raw_data_bytes, dtype=np.uint8)
-    len_of_data = len(data_bytes)
-    num_bytes_per_row = int(num_cols_per_frame * 3 / 8)
-    num_bytes_per_frame = num_bytes_per_row * num_rows_per_frame
+    with open(infile_path, 'rb') as fd:
+        raw_data_bytes = fd.read()
+        if encrypt:
+            raw_data_bytes = encrypt_data_aes(raw_data_bytes, key)
+        data_bytes = np.frombuffer(raw_data_bytes, dtype=np.uint8)
+        len_of_data = len(data_bytes)
+        num_bytes_per_row = int(num_cols_per_frame * 3 / 8)
+        num_bytes_per_frame = num_bytes_per_row * num_rows_per_frame
 
-    len_bytes = np.array(bytearray(len_of_data.to_bytes(4, byteorder='big')), dtype=np.uint8)
-    (num_frames, num_leftover_bytes) = quotient_remainder(4 + len_of_data, num_bytes_per_frame)
+        len_bytes = np.array(bytearray(len_of_data.to_bytes(4, byteorder='big')), dtype=np.uint8)
+        (num_frames, num_leftover_bytes) = quotient_remainder(4 + len_of_data, num_bytes_per_frame)
 
-    if num_leftover_bytes > 0:
-        num_bytes_last_frame_padding = num_bytes_per_frame - num_leftover_bytes
-        padding_bytes = np.full((num_bytes_last_frame_padding), 0, dtype=np.uint8)
-        data_bytes = np.concatenate((len_bytes, data_bytes, padding_bytes))
-        num_frames += 1
-    else:
-        data_bytes = np.concatenate((len_bytes, data_bytes))
+        if num_leftover_bytes > 0:
+            num_bytes_last_frame_padding = num_bytes_per_frame - num_leftover_bytes
+            padding_bytes = np.full((num_bytes_last_frame_padding), 0, dtype=np.uint8)
+            data_bytes = np.concatenate((len_bytes, data_bytes, padding_bytes))
+            num_frames += 1
+        else:
+            data_bytes = np.concatenate((len_bytes, data_bytes))
 
-    # Vedio: size=(1280, 720), fps=20
-    size = (num_cols_per_frame * 20, num_rows_per_frame * 20)
+        # Vedio: size=(1280, 720), fps=20
+        size = (num_cols_per_frame * 20, num_rows_per_frame * 20)
 
-    video = cv2.VideoWriter(outvideo_path, cv2.VideoWriter_fourcc(
-        *'mp4v'), fps, size)
+        video = cv2.VideoWriter(outvideo_path, cv2.VideoWriter_fourcc(
+            *'mp4v'), fps, size)
 
-    for i in range(num_frames):
-        frame_bytes = data_bytes[i * num_bytes_per_frame: (i + 1) * num_bytes_per_frame]
-        frame_bits = np.unpackbits(frame_bytes)
-        # Reshape array to array(36， 72， 3)
-        frame = color_value(frame_bits).reshape(num_rows_per_frame, num_cols_per_frame, 3)
-        img = Image.fromarray(frame, mode="RGB")
-        # Scale image to (1280, 720)
-        newimg = img.resize(size, Image.Resampling.BOX)
-        video.write(np.asarray(newimg))
-    fd.close()
+        for i in range(num_frames):
+            frame_bytes = data_bytes[i * num_bytes_per_frame: (i + 1) * num_bytes_per_frame]
+            frame_bits = np.unpackbits(frame_bytes)
+            # Reshape array to array(36， 72， 3)
+            frame = color_value(frame_bits).reshape(num_rows_per_frame, num_cols_per_frame, 3)
+            img = Image.fromarray(frame, mode="RGB")
+            # Scale image to (1280, 720)
+            newimg = img.resize(size, Image.Resampling.BOX)
+            video.write(np.asarray(newimg))
 
 
 def decode(invideo_path, outfile_path, decrypt=ENABLE_ENCRYPTION, key=KEY):
@@ -87,7 +85,7 @@ def decode(invideo_path, outfile_path, decrypt=ENABLE_ENCRYPTION, key=KEY):
         frame_shape = np.shape(frame)
         for j in range(0, frame_shape[0], step):
             for k in range(0, frame_shape[1], step):
-                piece = frame[j:j+step, k:k+step].reshape(step*step, 3)
+                piece = frame[j:j+step, k:k+step].reshape(step**2, 3)
                 round = normal(piece.mean(0)).round().astype(np.uint8)
                 data_bits_list.append(round)
         ret, frame = cap.read()
@@ -99,12 +97,7 @@ def decode(invideo_path, outfile_path, decrypt=ENABLE_ENCRYPTION, key=KEY):
     data_bytes_retrieved = data_bytes[4:len_of_data+4].tobytes()
     if decrypt:
         data_bytes_retrieved = decrypt_data_aes(data_bytes_retrieved, key)
-    fd = open(outfile_path, 'wb')
-    fd.write(data_bytes_retrieved)
-    fd.close()
+    with open(outfile_path, 'wb') as fd:
+        fd.write(data_bytes_retrieved)
 
 
-if __name__ == '__main__':
-    # encode("../examples/painting.jpg", "../examples/upload.mp4")
-    # decode("../examples/upload.mp4", "../examples/painting-retrieved2.jpg")
-    pass
